socialabs/emongo

commit 11588e94dfd89c42ecbef5f33b9c02f85bf4948c

Fix the 001-load test by filtering pools.
Signed-off-by: François de Metz <fdemetz@af83.com>

src/emongo_sup.erl
@@ -52,7 +52,7 @@ stop_pool(PoolId) ->
 
 pools() ->
     [{Id, Pid, Module} || {Id, Pid, _, [Module]}
-                              <- supervisor:which_children(?MODULE)].
+                              <- supervisor:which_children(?MODULE), Module == emongo_pool].
 
 worker_pid(PoolId, Pools) ->
     worker_pid(PoolId, Pools, 1).

commit 07af93709fe88b51feefc2281147576a5778b574

emongo_sup:pools() should return not only emongo_pools, but emongo_routers as well.
Was broken by 11588e94dfd89c42ecbef5f33b9c02f85bf4948c.

src/emongo_sup.erl
@@ -52,7 +52,7 @@ stop_pool(PoolId) ->
 
 pools() ->
     [{Id, Pid, Module} || {Id, Pid, _, [Module]}
-                              <- supervisor:which_children(?MODULE), Module == emongo_pool].
+                              <- supervisor:which_children(?MODULE), Module /= emongo].
 
 worker_pid(PoolId, Pools) ->
     worker_pid(PoolId, Pools, 1).